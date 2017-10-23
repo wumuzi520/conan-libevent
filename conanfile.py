2d1e20c0a4c14c4b16a760097efeead46a069309
theirix <theirix@gmail.com>
2017-10-23 17:00:06 +0300

    Keep options to easy porting to 2.1

--- conanfile.py
+++ conanfile.py
@@ -18,8 +18,8 @@ class LibeventConan(ConanFile):
     def config_options(self):
         del self.settings.compiler.libcxx
         if self.settings.os == "Windows":
-            del self.options.shared
-            del self.options.with_openssl
+            self.options.shared = False
+            self.options.with_openssl = False
 
     def requirements(self):
         if self.options.with_openssl:
